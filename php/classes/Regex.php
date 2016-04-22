c9a20ae509efc0372b854fa61ea3969e16bc346a
Patrick Bitonti <patrick.bitonti@backatyou.com>
2016-04-22 16:49:09 -0700

    update image regex to ignore image file extensions and match whatever is inside the image src

--- php/classes/Regex.php
+++ php/classes/Regex.php
@@ -20,7 +20,7 @@ class Regex
     //$urlRegex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor
 
     public static $urlRegex = "/((https?|ftp)\:\/\/)?([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?([a-z0-9-.]*)\.([a-z]{2,3})(\:[0-9]{2,5})?(\/([a-z0-9+\$_\-~@\(\)\%]\.?)+)*\/?(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?(#[a-z_.-][a-z0-9+\$_.-]*)?/i";
-    public static $imageRegex = "/<img(.*?)src=(\"|\')(.+?)(gif|jpg|png|bmp)(.*?)(\"|\')(.*?)(\/)?>(<\/img>)?/";
+    public static $imageRegex = "/<img(.*?)src=(\"|\')(.+?)(\"|\')(.*?)(\/)?>(<\/img>)?/";
     public static $imagePrefixRegex = "/\.(jpg|png|gif|bmp)$/i";
     public static $srcRegex = '/src=(\"|\')(.+?)(\"|\')/i';
     public static $httpRegex = "/https?\:\/\//i";
