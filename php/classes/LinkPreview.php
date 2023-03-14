a76e4e062cefe6d9a50eff8d321656b29fda656f
Patrick Bitonti <patrick.bitonti@backatyou.com>
2023-03-13 19:29:51 -0700

    handle vimeo link where thumbnails don't exist and iframe embed doesn't work

--- php/classes/LinkPreview.php
+++ php/classes/LinkPreview.php
@@ -19,104 +19,112 @@ include_once "Json.php";
 
 class LinkPreview
 {
-    function __construct(){}
+	function __construct(){}
 
-    function joinAll($matching, $number, $url, $content)
-    {
-        for ($i = 0; $i < count($matching[$number]); $i++) {
-            $imgSrc = $matching[$number][$i] . $matching[$number + 1][$i];
-            $src = "";
-            $pathCounter = substr_count($imgSrc, "../");
-            if (!preg_match(Regex::$httpRegex, $imgSrc)) {
-                $src = Url::getImageUrl($pathCounter, Url::canonicalLink($imgSrc, $url));
-            }
-            if ($src . $imgSrc != $url) {
-                if ($src == "")
-                    array_push($content, $src . $imgSrc);
-                else
-                    array_push($content, $src);
-            }
-        }
-        return $content;
-    }
+	function joinAll($matching, $number, $url, $content) {
+		for ($i = 0; $i < count($matching[$number]); $i++) {
+			$imgSrc = $matching[$number][$i] . $matching[$number + 1][$i];
+			$src = "";
+			$pathCounter = substr_count($imgSrc, "../");
+			if (!preg_match(Regex::$httpRegex, $imgSrc)) {
+				$src = Url::getImageUrl($pathCounter, Url::canonicalLink($imgSrc, $url));
+			}
+			if ($src . $imgSrc != $url) {
+				if ($src == "") {
+					array_push($content, $src . $imgSrc);
+				} else {
+					array_push($content, $src);
+				}
+			}
+		}
+		return $content;
+	}
 
-    function crawl($text, $imageQuantity, $header)
-    {
-        if (preg_match(Regex::$urlRegex, $text, $match)) {
-            $title = "";
-            $description = "";
-            $videoIframe = "";
-            $video = "no";
+	function crawl($text, $imageQuantity, $header) {
+		$match = [];
+		if (preg_match(Regex::$urlRegex, $text, $match)) {
+			$title = "";
+			$description = "";
+			$videoIframe = "";
+			$video = "no";
 
-            //if (strpos($match[0], " ") === 0)
-            //    $match[0] = "http://" . substr($match[0], 1);
+			//if (strpos($match[0], " ") === 0)
+			//	$match[0] = "http://" . substr($match[0], 1);
 
-            $finalUrl = $text;
-            $pageUrl = str_replace("https://", "http://", $finalUrl);
+			$finalUrl = $text;
+			$pageUrl = str_replace("https://", "http://", $finalUrl);
 
 			$images = [];
-            if (Content::isImage($pageUrl)) {
-                $images[] = $pageUrl;
-            } else {
-                $urlData = $this->getPage($pageUrl);
-                if (!$urlData["content"] && strpos($pageUrl, "//www.") === false) {
-                    if (strpos($pageUrl, "http://") !== false)
-                        $pageUrl = str_replace("http://", "http://www.", $pageUrl);
-                    elseif (strpos($pageUrl, "https://") !== false)
-                        $pageUrl = str_replace("https://", "https://www.", $pageUrl);
+			if (Content::isImage($pageUrl)) {
+				$images[] = $pageUrl;
+			} else {
+				$urlData = $this->getPage($pageUrl);
+				if (!$urlData["content"] && strpos($pageUrl, "//www.") === false) {
+					if (strpos($pageUrl, "http://") !== false) {
+						$pageUrl = str_replace("http://", "http://www.", $pageUrl);
+					} elseif (strpos($pageUrl, "https://") !== false) {
+						$pageUrl = str_replace("https://", "https://www.", $pageUrl);
+					}
 
-                    $urlData = $this->getPage($pageUrl);
-                }
+					$urlData = $this->getPage($pageUrl);
+				}
 
-                $pageUrl = $finalUrl = $urlData["url"];
-                $raw = $urlData["content"];
-                $header = $urlData["header"];
-				$headers = $urlData["headers"];
+				$pageUrl = $finalUrl = $urlData["url"];
+				$raw = $urlData["content"];
+				$header = $urlData["header"];
 
-                $metaTags = Content::getMetaTags($raw);
+				$metaTags = Content::getMetaTags($raw);
 
-                $tempTitle = Content::extendedTrim($metaTags["title"]);
-                if ($tempTitle != "")
-                    $title = $tempTitle;
+				$tempTitle = Content::extendedTrim($metaTags["title"]);
+				if ($tempTitle != "") {
+					$title = $tempTitle;
+				}
 
-                if ($title == "") {
-                    if (preg_match(Regex::$titleRegex, str_replace("\n", " ", $raw), $matching))
-                        $title = $matching[2];
-                }
+				if ($title == "") {
+					$matching = [];
+					if (preg_match(Regex::$titleRegex, str_replace("\n", " ", $raw), $matching)) {
+						$title = $matching[2];
+					}
+				}
 
-                $tempDescription = Content::extendedTrim($metaTags["description"]);
-                if ($tempDescription != "")
-                    $description = $tempDescription;
-                else
-                    $description = Content::crawlCode($raw);
+				$tempDescription = Content::extendedTrim($metaTags["description"]);
+				if ($tempDescription != "") {
+					$description = $tempDescription;
+				} else {
+					$description = Content::crawlCode($raw);
+				}
 
-                $descriptionUnderstood = false;
-                if ($description != "")
-                    $descriptionUnderstood = true;
+				$descriptionUnderstood = false;
+				if ($description != "") {
+					$descriptionUnderstood = true;
+				}
 
-                if (($descriptionUnderstood == false && strlen($title) > strlen($description) && !preg_match(Regex::$urlRegex, $description) && $description != "" && !preg_match('/[A-Z]/', $description)) || $title == $description) {
-                    $title = $description;
-                    $description = Content::crawlCode($raw);
-                }
+				if (($descriptionUnderstood == false && strlen($title) > strlen($description) && !preg_match(Regex::$urlRegex, $description) && $description != "" && !preg_match('/[A-Z]/', $description)) || $title == $description) {
+					$title = $description;
+					$description = Content::crawlCode($raw);
+				}
 
-                if(Content::isJson($title)){
-                    $title = "";
-                }
-                if(Content::isJson($description)){
-                    $description = "";
-                }
+				if(Content::isJson($title)){
+					$title = "";
+				}
+				if(Content::isJson($description)){
+					$description = "";
+				}
 
-                $media = self::getMedia($pageUrl);
-                if(count($media) == 0) {
-                	foreach($metaTags['images'] as $metaImage) {
-                		$images[] = !preg_match(Regex::$httpRegex, $metaImage) ? Url::canonicalLink(Content::extendedTrim($metaImage), $pageUrl) : $metaImage;
-                	}
-                } else {
-                	$images[] = $media[0];
+				$media = self::getMedia($pageUrl);
+				if(count($media) === 0) {
+					foreach($metaTags['images'] as $metaImage) {
+						$images[] = !preg_match(Regex::$httpRegex, $metaImage) ? Url::canonicalLink(Content::extendedTrim($metaImage), $pageUrl) : $metaImage;
+					}
+				} else if(count($media) === 2) {
+					if(!empty($media[0])) {
+						$images[] = $media[0];
+					}
 					$videoIframe = $media[1];
-                }
-				if ($media != null && $media[0] != "" && $media[1] != "")
-                    $video = "yes";
+				}
+				if ($media != null && $media[0] != "" && $media[1] != "") {
+					$video = "yes";
+				}
 
 				if($imageQuantity == 0) {
 					$images = [];
@@ -125,75 +133,78 @@ class LinkPreview
 					$images = array_keys(array_flip($images));// filter out duplicate image urls
 				}
 
-                $title = Content::extendedTrim($title);
-                $pageUrl = Content::extendedTrim($pageUrl);
-                $description = Content::extendedTrim($description);
+				$title = Content::extendedTrim($title);
+				$pageUrl = Content::extendedTrim($pageUrl);
+				$description = Content::extendedTrim($description);
 
-                $description = preg_replace(Regex::$scriptRegex, "", $description);
-            }
+				$description = preg_replace(Regex::$scriptRegex, "", $description);
+			}
 
-            $finalLink = explode("&", $finalUrl);
-            $finalLink = $finalLink[0];
+			$finalLink = explode("&", $finalUrl);
+			$finalLink = $finalLink[0];
 
-            $description = strip_tags($description);
+			$description = strip_tags($description);
 
-            $answer = array("title" => $title, "url" => $finalLink, "pageUrl" => $finalUrl, "canonicalUrl" => Url::canonicalPage($pageUrl), "description" => $description,
-                "images" => $images, "video" => $video, "videoIframe" => $videoIframe);
+			$answer = [
+				"title" => $title,
+				"url" => $finalLink,
+				"pageUrl" => $finalUrl,
+				"canonicalUrl" => Url::canonicalPage($pageUrl),
+				"description" => $description,
+				"images" => $images,
+				"video" => $video,
+				"videoIframe" => $videoIframe
+			];
 
-            $result_json = Json::jsonSafe($answer, $header);
-            $result_json_decoded = json_decode($result_json);
+			$result_json = Json::jsonSafe($answer, $header);
+			$result_json_decoded = json_decode($result_json);
 
-            $flagged = false;
+			$flagged = false;
 
-            if (!isset($result_json_decoded->title)) {
-                $title = utf8_encode($title);
-                $flagged = true;
-            }
+			if (!isset($result_json_decoded->title)) {
+				$answer['title'] = utf8_encode($title);
+				$flagged = true;
+			}
 
-            if (!isset($result_json_decoded->description)) {
-                $description = utf8_encode($description);
-                $flagged = true;
-            }
+			if (!isset($result_json_decoded->description)) {
+				$answer['description'] = utf8_encode($description);
+				$flagged = true;
+			}
 
-            if ($flagged) {
-                $answer = array("title" => $title, "url" => $finalLink, "pageUrl" => $finalUrl, "canonicalUrl" => Url::canonicalPage($pageUrl), "description" => $description,
-                    "images" => $images, "video" => $video, "videoIframe" => $videoIframe);
+			if ($flagged) {
+				return Json::jsonSafe($answer, $header);
+			}
+			return $result_json;
+		}
+		return null;
+	}
 
-                return Json::jsonSafe($answer, $header);
-            } else {
-                return $result_json;
-            }
-        }
-        return null;
-    }
-
-    function getPage($url)
-    {
-        $res = array();
-        $options = array(
+	function getPage($url) {
+		$res = [];
+		$options = [
 			CURLOPT_SSL_VERIFYPEER => false,//FALSE to stop cURL from verifying the peer's certificate
 			CURLOPT_RETURNTRANSFER => true, // return web page
-            CURLOPT_HEADER => true, // return headers
-            CURLOPT_FOLLOWLOCATION => true, // follow redirects
-            CURLOPT_USERAGENT => "BAY-LinkPreviewer/1.0",
-            CURLOPT_AUTOREFERER => true, // set referer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
-            CURLOPT_TIMEOUT => 120, // timeout on response
-            CURLOPT_MAXREDIRS => 10, // stop after 10 redirects
-            CURLOPT_ENCODING => "utf-8",// sets "Accept-Encoding: " header to all supported encoding types
+			CURLOPT_HEADER => true, // return headers
+			CURLOPT_FOLLOWLOCATION => true, // follow redirects
+			CURLOPT_USERAGENT => "BAY-LinkPreviewer/1.0",
+			CURLOPT_AUTOREFERER => true, // set referer on redirect
+			CURLOPT_CONNECTTIMEOUT => 120, // timeout on connect
+			CURLOPT_TIMEOUT => 120, // timeout on response
+			CURLOPT_MAXREDIRS => 10, // stop after 10 redirects
+			CURLOPT_ENCODING => "utf-8",// sets "Accept-Encoding: " header to all supported encoding types
 			CURLOPT_REFERER => "https://www.backatyou.com"// set Referer: header
-        );
-        $ch = curl_init($url);
-        curl_setopt_array($ch, $options);
-        $content = curl_exec($ch);
-        $header = curl_getinfo($ch);
-        curl_close($ch);
+		];
+		$ch = curl_init($url);
+		curl_setopt_array($ch, $options);
+		$content = curl_exec($ch);
+		$header = curl_getinfo($ch);
+		curl_close($ch);
 
-        $curlHeaderSize=$header['header_size'];
+		$curlHeaderSize=$header['header_size'];
 		$body = trim(mb_substr($content, $curlHeaderSize));
 		$ResponseHeader = explode("\n",trim(mb_substr($content, 0, $curlHeaderSize)));
 		unset($ResponseHeader[0]);
-		$aHeaders = array();
+		$aHeaders = [];
 		foreach($ResponseHeader as $line){
 			if (strpos($line,':') != false) {
 				list($key,$val) = explode(':',$line,2);
@@ -201,37 +212,36 @@ class LinkPreview
 			}
 		}
 
-        $hrd = $header["content_type"];
-        //header("Content-Type: " . $hrd, true);
+		$hrd = $header["content_type"];
+		//header("Content-Type: " . $hrd, true);
 
-        $res['content'] = $body;
-        $res['url'] = $header['url'];
-        $res['header'] = $hrd;
+		$res['content'] = $body;
+		$res['url'] = $header['url'];
+		$res['header'] = $hrd;
 		$res['headers'] = $aHeaders;
-        return $res;
-    }
+		return $res;
+	}
 
-    public static function getMedia($pageUrl)
-    {
-        $media = array();
-        if (strpos($pageUrl, "youtube.com") !== false || strpos($pageUrl, "youtu.be") !== false) {
-            $media = Media::mediaYoutube($pageUrl);
-        } else if (strpos($pageUrl, "vimeo.com") !== false) {
-            $media = Media::mediaVimeo($pageUrl);
-        } else if (strpos($pageUrl, "vine.co") !== false) {
-            $media = Media::mediaVine($pageUrl);
-        } else if (strpos($pageUrl, "metacafe.com") !== false) {
-            $media = Media::mediaMetacafe($pageUrl);
-        } else if (strpos($pageUrl, "dailymotion.com") !== false) {
-            $media = Media::mediaDailymotion($pageUrl);
-        } else if (strpos($pageUrl, "collegehumor.com") !== false) {
-            $media = Media::mediaCollegehumor($pageUrl);
-        } else if (strpos($pageUrl, "blip.tv") !== false) {
-            $media = Media::mediaBlip($pageUrl);
-        } else if (strpos($pageUrl, "funnyordie.com") !== false) {
-            $media = Media::mediaFunnyordie($pageUrl);
-        }
-        return $media;
-    }
+	public static function getMedia($pageUrl) {
+		$media = [];
+		if (strpos($pageUrl, "youtube.com") !== false || strpos($pageUrl, "youtu.be") !== false) {
+			$media = Media::mediaYoutube($pageUrl);
+		} else if (strpos($pageUrl, "vimeo.com") !== false) {
+			$media = Media::mediaVimeo($pageUrl);
+		} else if (strpos($pageUrl, "vine.co") !== false) {
+			$media = Media::mediaVine($pageUrl);
+		} else if (strpos($pageUrl, "metacafe.com") !== false) {
+			$media = Media::mediaMetacafe($pageUrl);
+		} else if (strpos($pageUrl, "dailymotion.com") !== false) {
+			$media = Media::mediaDailymotion($pageUrl);
+		} else if (strpos($pageUrl, "collegehumor.com") !== false) {
+			$media = Media::mediaCollegehumor($pageUrl);
+		} else if (strpos($pageUrl, "blip.tv") !== false) {
+			$media = Media::mediaBlip($pageUrl);
+		} else if (strpos($pageUrl, "funnyordie.com") !== false) {
+			$media = Media::mediaFunnyordie($pageUrl);
+		}
+		return $media;
+	}
 }
 ?>
